b06ab6a1d259313a0f8cb47880eb9e36d024481a
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-26 19:04:21 +0530

    binary search tree

---
+++ src/main/java/bNode.java
@@ -0,0 +1,12 @@
+class bNode<K extends Comparable<K>> {
+    K key;
+    bNode<K> left;
+    bNode<K> right;
+
+    bNode(K key)
+    {
+        this.key=key;
+        left=null;
+        right=null;
+    }
+}
---
+++ src/test/java/BSTTest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class BSTTest {
+    @Test
+    public void givenNodes_shouldReturnBST() {
+        BinaryST bst = new BinaryST();
+        bst.add(51);
+        bst.add(32);
+        bst.add(75);
+        int size = bst.getSize();
+        Assert.assertEquals(3,size);
+    }
+
+
+}
